884ea44e2d80e9ff7cd2dfb95005e2157906f8e3
chairayhuo <chairayhuo@gmail.com>
2024-04-09 01:31:16 -0400

    connect mongodb

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/huoxiaoxin/Desktop/Wasim/XXHaven_Multiverse/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4gahgRWn55hQR8CrkUkg8g0BRLuy6+pROgh47PHCXCg=\"\n}"
