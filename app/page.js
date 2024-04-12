c7155672bb839b51151303372ffe8588912d9700
gloriacao1018 <caoxiao@sheridancollege.ca>
2024-04-12 14:18:30 -0400

    k

--- app/page.js
+++ app/page.js
@@ -10,8 +10,8 @@ import {
   handleSubmit,
 } from "./script";
 import logo from "../app/assets/Landie.png";
-import foot from "../app/assets/bottom.png";
 import image from "../app/assets/Picture.png";
+import down from "../app/assets/down.png";
 
 export default function Home() {
   return (
@@ -63,15 +63,17 @@ export default function Home() {
                 </form>
               </div>
             </div>
-            <div className="button-container">
+            {/* <div className="button-container">
               <button className="cancel-button">Cancel</button>
               <button className="done-button">Done</button>
-            </div>
+            </div> */}
           </div>
         </div>
 
+        <Image src={down} alt="down arrow" className="down" />
         <h1 className="font-roboto text-9xl">STEP 2. CREATE YOUR CHARACTERS</h1>
-        <div className="box">
+        
+        <div className="box flex flex-col px-4">
           <label htmlFor="character1">Upload Image for Character 1: </label>
           <div id="item-drop-area">
             <form class="my-form">
@@ -81,24 +83,34 @@ export default function Home() {
                 name="character1"
                 onChange={handleImageUpload}
               />{" "}
-              <label class="button" for="fileElem">
-                Upload
-              </label>
+              <label class="add-button" for="fileElem"></label>
             </form>
           </div>
 
           <label htmlFor="character1Items">Accepted Items (Choose 3)</label>
-          <select
-            multiple
-            id="character1Items"
-            name="character1Items"
-            className="dropdown-menu closed px-3 py-2"
-          >
-            <option value="item1">Item 1</option>
-            <option value="item2">Item 2</option>
-            <option value="item3">Item 3</option>
-            <option value="item4">Item 4</option>
-          </select>
+          <div className="dropdown" style={{
+            minWidth: '350px',
+            width: '100%',
+            fontSize: '1.15rem',
+            padding: '0.675em 6em 0.675em 1em',
+            backgroundColor: '#fff',
+            border: '1px solid #caced1',
+            borderRadius: '0.25rem',
+            color: '#000',
+            cursor: 'pointer'
+          }}>
+              <select
+                multiple
+                id="character1Items"
+                name="character1Items"
+              >
+                <option value="item1">Item 1</option>
+                <option value="item2">Item 2</option>
+                <option value="item3">Item 3</option>
+                <option value="item4">Item 4</option>
+              </select>
+            </div>
+
 
           <label htmlFor="character2">Upload Image for Character 2: </label>
           <div id="item-drop-area">
@@ -109,9 +121,7 @@ export default function Home() {
                 name="character2"
                 onChange={handleImageUpload}
               />{" "}
-              <label class="button" for="fileElem">
-                Upload
-              </label>
+              <label class="add-button" for="fileElem"></label>
             </form>
           </div>
 
@@ -137,19 +147,13 @@ export default function Home() {
                 name="character3"
                 onChange={handleImageUpload}
               />{" "}
-              <label class="button" for="fileElem">
-                Upload
-              </label>
+              <label class="add-button" for="fileElem"></label>
             </form>
           </div>
 
           <label htmlFor="character1Items">Accepted Items (Choose 3)</label>
-          <select
-            multiple
-            id="character3Items"
-            name="character3Items"
-            className="dropdown-menu px-3 py-2"
-          >
+
+          <select multiple id="character3Items" name="character3Items">
             <option value="item1">Item 1</option>
             <option value="item2">Item 2</option>
             <option value="item3">Item 3</option>
@@ -165,9 +169,7 @@ export default function Home() {
                 name="character4"
                 onChange={handleImageUpload}
               />{" "}
-              <label class="button" for="fileElem">
-                Upload
-              </label>
+              <label class="add-button" for="fileElem"></label>
             </form>
           </div>
 
@@ -190,7 +192,7 @@ export default function Home() {
         </h1>
 
         <div className="box">
-          <div className="flex w-1/5">
+          <div className="item-box flex flex-col 1/4">
             <label htmlFor="itemImage2">Item1:</label>
             <div id="item-drop-area">
               <form class="my-form">
@@ -200,14 +202,12 @@ export default function Home() {
                   name="item1"
                   onChange={handleImageUpload}
                 />{" "}
-                <label class="button" for="fileElem">
-                  Upload
-                </label>
+                <label class="add-button" for="fileElem"></label>
               </form>
             </div>
           </div>
 
-          <div className="flex w-1/5">
+          <div className="item-box flex flex-col 1/4">
             <label htmlFor="itemImage2">Item2:</label>
             <div id="item-drop-area">
               <form class="my-form">
@@ -217,14 +217,12 @@ export default function Home() {
                   name="item2"
                   onChange={handleImageUpload}
                 />{" "}
-                <label class="button" for="fileElem">
-                  Upload
-                </label>
+                <label class="add-button" for="fileElem"></label>
               </form>
             </div>
           </div>
 
-          <div className="flex w-1/5">
+          <div className="item-box flex flex-col 1/4">
             <label htmlFor="itemImage3">Item3: </label>
             <div id="item-drop-area">
               <form class="my-form">
@@ -234,14 +232,12 @@ export default function Home() {
                   name="item3"
                   onChange={handleImageUpload}
                 />{" "}
-                <label class="button" for="fileElem">
-                  Upload
-                </label>
+                <label class="add-button" for="fileElem"></label>
               </form>
             </div>
           </div>
 
-          <div className="flex w-1/5">
+          <div className="item-box flex flex-col 1/4">
             <label htmlFor="itemImage4">Item4: </label>
             <div id="item-drop-area">
               <form class="my-form">
@@ -251,68 +247,69 @@ export default function Home() {
                   name="item4"
                   onChange={handleImageUpload}
                 />{" "}
-                <label class="button" for="fileElem">
-                  Upload
-                </label>
+                <label class="add-button" for="fileElem"></label>
               </form>
             </div>
           </div>
         </div>
 
-        <h1 className="font-roboto text-9xl font-medium">
-          STEP 4. TIMER LENGTH
-        </h1>
-        <input
-          className="timer border-gray-300"
-          type="number"
-          id="timerLength"
-          name="timerLength"
-          min="1"
-          max="240"
-        />
-
-        <h1 className="font-roboto text-9xl font-medium">
-          STEP 5. GIVE THE WINNER SOME COMPLIMENTS uwu
-        </h1>
-
-        <div className=" flex flex-col">
-          <label htmlFor="winnerScenario" className="achievements">
-            Achievements: 0
-          </label>
-          <textarea
-            id="winnerScenario1"
-            name="winnerScenario1"
-            rows="4"
-            cols="50"
-          ></textarea>
-
-          <label htmlFor="winnerScenario" className="achievements">
-            Achievements: 20
-          </label>
-          <textarea
-            id="winnerScenario2"
-            name="winnerScenario2"
-            rows="4"
-            cols="50"
-          ></textarea>
-
-          <label htmlFor="winnerScenario3" className="achievements">
-            Achievements: 20+
-          </label>
-          <textarea
-            id="winnerScenario3"
-            name="winnerScenario3"
-            rows="4"
-            cols="50"
-          ></textarea>
+        <div className="mount bg-center bg-no-repeat bg-cover bg-fixed bg-heroImage">
+          <h1 className="font-roboto text-9xl font-medium">
+            STEP 4. TIMER LENGTH
+          </h1>
+          <input
+            className="timer border-gray-300"
+            type="number"
+            id="timerLength"
+            name="timerLength"
+            min="1"
+            max="240"
+          />
         </div>
-        <button
-          type="submit"
-          className="submit align-middle w-40 h-16 rounded-lg"
-        >
-          VOILA
-        </button>
-        <Image className="bottom" src={foot} alt="bottom picture" />
+
+        <div className="bottom bg-center bg-no-repeat bg-cover bg-fixed bg-heroImage">
+          <h1 className="font-roboto text-9xl font-medium">
+            STEP 5. GIVE THE WINNER SOME COMPLIMENTS uwu
+          </h1>
+
+          <div className=" flex flex-col">
+            <label htmlFor="winnerScenario" className="achievements">
+              Achievements: 0
+            </label>
+            <textarea
+              id="winnerScenario1"
+              name="winnerScenario1"
+              rows="4"
+              cols="50"
+            ></textarea>
+
+            <label htmlFor="winnerScenario" className="achievements">
+              Achievements: 20
+            </label>
+            <textarea
+              id="winnerScenario2"
+              name="winnerScenario2"
+              rows="4"
+              cols="50"
+            ></textarea>
+
+            <label htmlFor="winnerScenario3" className="achievements">
+              Achievements: 20+
+            </label>
+            <textarea
+              id="winnerScenario3"
+              name="winnerScenario3"
+              rows="4"
+              cols="50"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="submit align-middle w-40 h-16 rounded-lg"
+          >
+            VOILA
+          </button>
+        </div>
       </form>
     </main>
   );
